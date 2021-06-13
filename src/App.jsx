802b5bfd09d04e00313553cbd4c6fc3637dd4fde
roshanyadav7389 <83867770+roshanyadav7389@users.noreply.github.com>
2021-06-14 00:59:22 +0530

    App.jsx file added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './index.css';
+// import Login from './Login';
+import LoginTest from './LoginTest';
+import Ragistration from './Ragistration';
+
+const App = () => {
+    return(
+         <>
+            <LoginTest/>
+            <Ragistration />
+            {/* <Login /> */}
+         </>
+    );
+};
+
+export default App;
